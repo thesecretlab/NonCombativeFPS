c21c17320de24e6c5fd120d39759aa144d67eb81
jpalbion <jpalbion@utas.edu.au>
2017-04-30 15:01:37 +1000

    Created core Interactable classes and player hook
    use "interface : Interactable"

---
+++ Solitude/Assets/Scripts/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+
+    public Vector3 getPos() {
+        return transform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.setup();
+        GameObject.FindGameObjectWithTag("Player").SendMessage("addInteractable", this);
+	}
+
+    protected abstract void setup();
+
+    public abstract void interact();
+}
---
+++ Solitude/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    List<Interactable> interfaces = new List<Interactable>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addInteractable(Interactable inter) {
+        interfaces.Add(inter);
+        inter.interact();
+    }
+}
