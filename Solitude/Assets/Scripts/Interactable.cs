84e2ef65b4c502c4e979d2579a1729d2ee8db4cd
jpalbion <jpalbion@utas.edu.au>
2017-05-08 15:48:56 +1000

    Copy Back

---
+++ Solitude/Assets/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+
+    private bool active = true;
+
+    public Vector3 getPos() {
+        return transform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.setup();
+        GameObject.FindGameObjectWithTag("Player").SendMessage("addInteractable", this);
+	}
+
+    public bool isActive() {
+        return active;  
+    }
+
+    protected abstract void setup();
+
+    public abstract void interact();
+}
