40f18da7c638872b7def259e1e3574ceca7ea321
jpalbion <jpalbion@utas.edu.au>
2017-05-13 13:09:37 +1000

    Fixed the terminal class

--- Solitude/Assets/Scripts/Terminal.cs
+++ Solitude/Assets/Scripts/Terminal.cs
@@ -3,17 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terminal : Interactable {
+public abstract class Terminal : Interactable {
     public GameObject uiPrefab;
-    private GameObject ui;
-    public override void interact() {
-        Debug.Log("Terminal Incorectly setup: " + transform.gameObject.name);
-    }
+    protected GameObject ui;
+    protected abstract void initialise();
+
+    public override abstract void interact();
 
     protected override void setup() {
         GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
         ui = Instantiate(uiPrefab);
         ui.transform.SetParent(canvas.transform, false);
         ui.SetActive(false);
+        this.initialise();
     }
+
+    protected void showUi() {
+        ui.SetActive(true);
+    }
+
+
 }
