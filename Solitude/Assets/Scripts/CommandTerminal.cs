486b330233f6c7ac850df20e802c2d8da31da857
Sam <sam.cowley431@gmail.com>
2017-05-23 14:18:02 +1000

    Command terminal changes
    command terminal changes 23/03

--- Solitude/Assets/Scripts/CommandTerminal.cs
+++ Solitude/Assets/Scripts/CommandTerminal.cs
@@ -8,6 +8,9 @@ public class CommandTerminal : Terminal, Breakable {
 
     public Text t;
     string line = "";
+    public Boolean service = true;
+    public Boolean navDir = true;
+    public Boolean isBroken = true;
 
     List<string> console = new List<string>();
     public List<string> commands = new List<string>();
@@ -16,6 +19,7 @@ public class CommandTerminal : Terminal, Breakable {
 
     public override void interact() {
         showUI(true);
+        navDir = false;
     }
 
     public void onBreak() {
@@ -66,15 +70,53 @@ public class CommandTerminal : Terminal, Breakable {
 
             case "cd /navigation":
                 addline("cd /navigation");
+                addline("moving to navigation directory");
+                navDir = true;
+
                 break;
             case "service stop navigation":
                 addline("service stop navigation");
+                if(service)
+                {
+                    addline("service stopping");
+                    service = false;
+                }
+                else if(!service)
+                {
+                    addline("service currently inactive");
+                }
+                else
+                {
+                    addline("An error has occured please try again");
+                }
                 break;
             case "mv navigation.back navigation":
                 addline("mv navigation.back navigation");
+                if(navDir && !service)
+                {
+                    addline("Restoring back up files, service will not resume until it is started again");
+                    isBroken = false;
+                }
+                else if(!navDir)
+                {
+                    addline("Have to be in navigation directory to use this command");
+                }
+                else
+                {
+                    addline("An error has occured please try again");
+                }
                 break;
             case "service start navigation":
                 addline("service start navigation");
+                if(service)
+                {
+                    addline("service is already started");
+                }
+                else if(!service)
+                {
+                    addline("starting service");
+                    service = true;
+                }
                 break;
             case "help":
                 addline("Available Commands");
