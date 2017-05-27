bc14388692354429a541e084cd78967530258402
jpalbion <jpalbion@utas.edu.au>
2017-05-27 20:52:37 +1000

    Initial Breaker Minigame

---
+++ Solitude/Assets/Scripts/Breaker/Lever.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : Interactable {
+    Animator anim;
+
+    Reactor rec;
+    public int num;
+
+    public override void interact() {
+        anim.SetTrigger("pull");
+        rec.throwLever(num);
+    }
+
+    public void setReactor(Reactor rec,int num) {
+        this.rec = rec;
+        this.num = num;
+    }
+
+    protected override void setup() {
+        anim = GetComponent<Animator>();
+    }
+}
---
+++ Solitude/Assets/Scripts/Breaker/Reactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reactor : MonoBehaviour, Breakable {
+    public Lever[] levers;
+
+    public void onBreak() {
+        throw new NotImplementedException();
+    }
+
+    public void onFix() {
+        throw new NotImplementedException();
+    }
+
+    public void throwLever(int lever) {
+        levers[lever].gameObject.SetActive(false);
+    }
+
+    void Start () {
+		levers = GetComponentsInChildren<Lever>();
+        int i = 0;
+        foreach (Lever lev in levers) {
+            lev.setReactor(this,i);
+            i++;
+            Debug.Log(i);
+        }
+	}
+}
