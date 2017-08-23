985f4808968e20f8927a3dd2ae2be42015131a6f
warglaivez <nieldj@utas.edu.au>
2017-08-23 14:04:41 +1000

    Hacking countdown
    Added countdown timer to minigame and created lose condition. Load main scene at lose condition.

---
+++ Solitude/Assets/Scripts/Hacking/CountDownTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float timeLeft = 5.0f;
+    float closetime = 3.0f;
+
+    public Text text;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Remaining:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            text.text = "#!HACK FAILED!#";
+            closetime -= Time.deltaTime;
+            SceneManager.LoadScene("MainGame");
+
+        }
+    }
+}
