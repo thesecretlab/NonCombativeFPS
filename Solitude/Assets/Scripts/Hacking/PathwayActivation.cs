b17d4a662fc1cce7856137d522be560e12c7c5f2
warglaivez <nieldj@utas.edu.au>
2017-08-23 21:24:26 +1000

    Hackingminigame scripts
    Added start node always true, activated line between buttons when both pressed, need to  stop onclick listeners that are out of reach of the current node space.

--- Solitude/Assets/Scripts/Hacking/PathwayActivation.cs
+++ Solitude/Assets/Scripts/Hacking/PathwayActivation.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PathwayActivation : MonoBehaviour {
-    
+
     public Button Button1;
     public Button Button2;
+
+    int button1_on;
+    int button2_on;
     public Image Pathway;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void Start() {
+        Button btn = Button1.GetComponent<Button>();
+        btn.onClick.AddListener(TaskButton1);
+    
+        Button btn2 = Button2.GetComponent<Button>();
+        btn2.onClick.AddListener(TaskButton2);
 
-        if (Button1 == true && Button2 == true)
+        if (Button1.gameObject.name == ("START"))
+        {
+            button1_on = 1;
+        }
+
+    }
+
+    void TaskButton1()
+    {
+        button1_on = 1;
+    }
+    void TaskButton2()
+    {
+        button2_on = 1;
+    }
+
+    public void Update()
+    {
+
+        if (button1_on == 1 && button2_on == 1)
         {
             Pathway.GetComponent<Image>().color = Color.cyan;
         }
-	}
+    }
 }
