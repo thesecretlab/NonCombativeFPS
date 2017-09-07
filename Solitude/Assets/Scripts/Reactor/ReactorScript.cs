a165ba96f21c47908a4ff2d6cbc235d297a1e197
Sam <sam.cowley431@gmail.com>
2017-09-07 17:30:01 +1000

    Reactor update
    Still trouble shooting issues, Having a hard time with button behavior and sliders.

--- Solitude/Assets/Scripts/Reactor/ReactorScript.cs
+++ Solitude/Assets/Scripts/Reactor/ReactorScript.cs
@@ -11,85 +11,144 @@ public class ReactorScript : MonoBehaviour {
     int powerUnits;
     Slider controlRod;
     Slider tempGage;
-    double heatInc;
+    float heatInc;
     Text status;
-    int online = 2; //0 offline, 1 pending, online 2
+    bool online;
+    bool cooling;
+    bool offline;
     GameObject shutdown;
+    Text tempNum;
+    Text cRodNum;
 
 	// Use this for initialization
 	void Start () {
 
-        powerUnits = 5;
+        online = false;
+        cooling = false;
+        offline = false;
+        heatInc = 0;
+        status = GameObject.Find("ReacStat").GetComponent<Text>();
+        tempGage = GameObject.Find("tempGage").GetComponent<Slider>();
+        powerUsage = GameObject.Find("PowerUnitVal").GetComponent<Text>();
+        controlRod = GameObject.Find("cRod").GetComponent<Slider>();
+        shutdown = GameObject.Find("EmerBut");
+        tempNum = GameObject.Find("tempNum").GetComponent<Text>();
+        cRodNum = GameObject.Find("cRodNum").GetComponent<Text>();
     }
 
-    public double increaseTemp(int powerOutput, int online)
+    public void increaseTemp(int powerOutput)
     {
+        
+        if (online)
+        {
+            if (powerOutput <= 4 && powerOutput > 0)
+            {
+                heatInc = heatInc + (float)0.05;
+                controlRod.value = 75;
+            }
 
-        if(powerOutput <= 4 && powerOutput > 0)
-        {
-            heatInc = heatInc + 0.5;
-            controlRod.value = 75;
-        }
-
-        else if(powerOutput > 4 && powerOutput <= 8)
-        {
-            heatInc = heatInc + 1;
-            controlRod.value = 50;
-        }
-        else if(powerOutput >= 9 && powerOutput < 12)
-        {
-            heatInc = heatInc + 2;
-            controlRod.value = 25;
-        }
-        else if (powerOutput >= 12)
-        {
-            heatInc = heatInc + 4;
-            controlRod.value = 0;
-        }
-        else if(online == 0)
-        {
-            heatInc = heatInc - 5;
-        }
-        else if (online == 1)
-        {
-            heatInc = heatInc - 2;
+            else if (powerOutput > 4 && powerOutput <= 8)
+            {
+                heatInc = heatInc + (float)0.1;
+                controlRod.value = 50;
+            }
+            else if (powerOutput >= 9 && powerOutput < 12)
+            {
+                heatInc = heatInc + (float)0.2;
+                controlRod.value = 25;
+            }
+            else if (powerOutput >= 12)
+            {
+                heatInc = heatInc + (float)0.4;
+                controlRod.value = 0;
+            }
         }
 
-        return heatInc;
+      
 
     }
+
+    public void medDraw()
+    {
+        powerUnits = 5;
+        powerUsage.text = powerUnits.ToString();
+        controlRod.value = 50;
+        cRodNum.text = controlRod.value.ToString();
+    }
+    public void liteDraw()
+    {
+        powerUnits = 2;
+        powerUsage.text = powerUnits.ToString();
+        controlRod.value = 75;
+        cRodNum.text = controlRod.value.ToString();
+    }
+
+    public void heavyDraw()
+    {
+        powerUnits = 10;
+        powerUsage.text = powerUnits.ToString();
+        controlRod.value = 25;
+        cRodNum.text = controlRod.value.ToString();
+    }
+
     public void powerUP()
     {
-        if (heatInc <= 0)
+        if (tempGage.value == 0)
         {
-            online = 2;
+            online = true;
+            cooling = false;
+            offline = false;
             status.text = "online";
+            liteDraw();
+            powerUsage.text = powerUnits.ToString();
+            controlRod.value = 75;
+            cRodNum.text = controlRod.value.ToString();
+            shutdown.GetComponent<Button>().interactable = true;
         }
     }
 
     public void ReactorOverload()
     {
-        online = 1;
-        status.text = "Offline";
-        powerUsage.text = "0";
+        online = false;
+        cooling = true;
+        offline = false;
         powerUnits = 0; //removed when set draw is implemented
         controlRod.value = 100;
+        cRodNum.text = controlRod.value.ToString();
+        powerUsage.text = powerUnits.ToString();
+        status.text = "Cooling";
+        shutdown.GetComponent<Button>().interactable = false;
     }
     
     public void EmergencyPowerDown()
     {
-        online = 0;
+
+        online = false;
+        cooling = false;
+        offline = true;
+
         powerUsage.text = "0";
         controlRod.value = 100;
+        cRodNum.text = controlRod.value.ToString();
         powerUnits = 0; //Remove when set draw is implemented
+        powerUsage.text = powerUnits.ToString();
+        status.text = "Offline";
+        shutdown.GetComponent<Button>().interactable = false;
+
+
+        Debug.Log("button value");
+        Debug.Log(online);
+
+
     }
 
     public void setDraw(int draw)
     {
-        if (online == 2)
+        if (online)
         {
             powerUnits = draw;
             powerUsage.text = draw.ToString();
+           
         }
 
     }
@@ -98,46 +157,64 @@ public class ReactorScript : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+
+        /*
+                if (online)
+                {
+                    Debug.Log("online");
+                    Debug.Log(online);
+                }
+                if (cooling)
+                {
+                    Debug.Log("cooling");
+                    Debug.Log(cooling);
+                }
+                if (offline)
+                {
+                    Debug.Log("offline");
+                    Debug.Log(offline);
+                }
+                */
+
+        tempNum.text = heatInc.ToString();
         
-        
-        
-
-        
-
-        status = GameObject.Find("ReacStat").GetComponent<Text>();
-        tempGage = GameObject.Find("tempGage").GetComponent<Slider>();
-        powerUsage = GameObject.Find("PowerUnitVal").GetComponent<Text>();
-        controlRod = GameObject.Find("cRod").GetComponent<Slider>();
-        shutdown = GameObject.Find("EmerBut");
-
-
-        
 
 
 
-        switch (online){
+        if (online)
+        {
+            Debug.Log("increasing heat");
+            increaseTemp(powerUnits);
+            tempGage.value = (float)heatInc;
+            tempNum.text = heatInc.ToString();
+            powerUsage.text = powerUnits.ToString();
+        }
+        if(!online)
+        {
+            if (cooling)
+            {
+                Debug.Log("decreasing heat");
+                heatInc = heatInc - (float)0.2;
+                tempGage.value = (float)heatInc;
+                tempNum.text = heatInc.ToString();
+                powerUsage.text = powerUnits.ToString();
+                Debug.Log("cooling");
+            }
+            else if (offline)
+            {
+                Debug.Log("decreasing heat");
+                heatInc = heatInc - (float)0.5;
+                tempNum.text = heatInc.ToString();
+                tempGage.value = (float)heatInc;
+                powerUsage.text = powerUnits.ToString();
+                Debug.Log("fast cooling");
 
-            case 0:
-                status.text = "offline";
-                shutdown.GetComponent<Button>().interactable = false;
-                break;
-            case 1:
-                status.text = "Cooling";
-                shutdown.GetComponent<Button>().interactable = false;
-                break;
-            case 2:
-                status.text = "Online";
-                powerUnits = 5;//removed when setDraw is implemented
-                shutdown.GetComponent<Button>().interactable = true;
-                break;
+            }
         }
 
 
-        heatInc = increaseTemp(powerUnits, online);
-        tempGage.value = (int)heatInc;
-        powerUsage.text = powerUnits.ToString();
 
-        if (online == 2)
+        if (online)
         {
 
 
@@ -145,19 +222,22 @@ public class ReactorScript : MonoBehaviour {
             if (tempGage.value >= 100)
             {
                 ReactorOverload();
-                online = 1;
             }
 
 
         }
-        else if(online != 2)
+        else if(!online)
         {
-            if (tempGage.value == 0)
+            if (tempGage.value <= 0)
             {
                 powerUP();
+                Debug.Log("powering up");
             }
         }
-     
+
+        tempNum.text = heatInc.ToString();
+
+
 
     }
 }
