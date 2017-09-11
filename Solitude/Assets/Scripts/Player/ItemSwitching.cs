a284d947a5433bec2282f26be2b1c0f33754b16d
Alex <AlexTheNinja007@hotmail.com>
2017-09-11 22:57:40 +1000

    6:  Inventory Item Buttons and Item Options Buttons
    Fixed dynamic display bug, changed item sprites to button, added onclick
    of items options buttons to swap or drop the item.
    +OnClickDrop Script handles onclick drop for item calls funtion in
    itemswitching script
    +OnClickSwap Script handles onclick Swap for item calls funtion in
    itemswitching script
    +Renamed placeholder images to show ID order
    +Added options object consisting of the two buttons (drop and swap) for
    item interation

--- Solitude/Assets/Scripts/Player/ItemSwitching.cs
+++ Solitude/Assets/Scripts/Player/ItemSwitching.cs
@@ -4,98 +4,103 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public struct item{
-	public int id { get; set; }		//Object Type
-	public int ammount { get; set; }//Ammount of object or left uses of obbject;
+	public int id { get; set; }			//Object Type
+	public int ammount { get; set; }	//Ammount of object or left uses of obbject;
+	public GameObject obj {get; set;}	//Sprite Image
 }
+	
 
+
+//code for item switching adapted from https://www.youtube.com/watch?v=Dn_BUIVdAPg&ab_channel=Brackeys
+//items should be made a child of the itemholder object on the player for this to work
+
+/*
+ * Inventory Script handles player item storage and selection. Items are stored as a struct in a 2D array matching rows and cols
+ * as they are displayed in game. The item sturct contains "id" representing the object type, "ammount" the ammount of the object
+ * and "obj" the object used to display the image of the icon. 
+ * The icons (sprites) are stored in "pictures" array with position corrisponding to "id". 
+ * 
+ * By Brendan
+ * Modified by Alexander Tilley 11/09/2017
+*/
 public class ItemSwitching : MonoBehaviour {
 
-    //code adapted from https://www.youtube.com/watch?v=Dn_BUIVdAPg&ab_channel=Brackeys
-    //items should be made a child of the itemholder object on the player for this to work
-	public GameObject row;
-	public GameObject[] display;
-	public Image[] items;
-	//public GameObject[] items;
-	public int xcords;
-	public int ycords;
-	public int zcords;
+    public int selectedItem = 0;					//
 
-    public int selectedItem = 0;
+	public Camera mainCam;							//Main Camera to make sure inventory fits screen (pre-Set Unity)
+	public GameObject options;						//Item Options to perform opertions (pre-set Unity)
+
+	public int sel_row_1;							//Selected Items rows and cols (To Hold)
+	public int sel_col_1;
+	public int sel_row_2;
+	public int sel_col_2;
+
+
 
 	private const int MAXSTACK = 20;				//Max number of stackable items
 	private const int INV_ROWS = 5;					//Inventory Rows
 	private const int INV_COLUMNS = 4;				//Inventory columns
 
-	//public struct item items[][];
-	public GameObject emptyImage;
-	public GameObject [,] visible = new GameObject[INV_ROWS,INV_COLUMNS];
-
-	public Sprite[] pictures = new Sprite[7];
-	public Sprite SCREWDRIVER_PIC;
-	public Sprite PLASMACUTTER_PIC;
-	public Sprite WELDER_PIC;
-
-
-
-
+	public GameObject emptyImage;					//Base Object Spirte (pre-set in Unity)
 
 	public item [,] inventory = new item[INV_ROWS,INV_COLUMNS];	//2D array of items
 
-	public const int EMPTYID = 0;					//Empty Inventory Slot ID
+	public const int EMPTY_ID = 			0;			//Empty Inventory Slot ID
 
-	public const int SCREWDRIVER = 	1;				//ScrewDriver ID
-	public const int PLASMACUTTER = 	2;			//PlasmaCutter ID
-	public const int WELDER = 			3;			//Welder ID
-	public const int POWERCABLE = 		4;			//PowerCable ID
-	public const int CIRCUITBOARD = 	5;			//Circuit Board ID
-	public const int SCRAPMETAL = 		6;			//Scrap Metal ID
-	public const int SCREWS = 			7;			//Screws ID
+	public const int SCREWDRIVER_ID = 		1;			//ScrewDriver ID
+	public const int PLASMACUTTER_ID = 		2;			//PlasmaCutter ID
+	public const int WELDER_ID = 			3;			//Welder ID
+	public const int POWERCABLE_ID = 		4;			//PowerCable ID
+	public const int CIRCUITBOARD_ID = 		5;			//Circuit Board ID
+	public const int SCRAPMETAL_ID = 		6;			//Scrap Metal ID
+	public const int SCREWS_ID = 			7;			//Screws ID
+
+	public Sprite[] pictures = new Sprite[8];		//Array of sprites corisponding to item ids (pics pre-set in Unity)
+
 	public float HEIGHT =  Screen.height;		//Window Height
 	public float WIDTH = Screen.width;			//Window Width
+
 	//public Vector3 NextPos = new Vector3(0.0f+(0.01f*WIDTH),0.0f+(HEIGHT-10.0f),1.0f);
 
 
 
     // Use this for initialization
     void Start () {
-		int hotBarOffSet = 0;
+		SelectItem();
 		//------------------------------------------------------------------------------------ Inventory Display Setup
+		WIDTH = mainCam.pixelWidth;
+		HEIGHT = mainCam.pixelHeight;
+		options.SetActive (false);
+
+		int hotBarOffSet = 0;
+		item blank = new item();							//Empty object to intialise
+
 		Debug.Log ("Screen Width: " + WIDTH + " Height: " + HEIGHT);
-        SelectItem();
-		item blank;
-		blank.ammount = 0;
-		blank.id = 0;
+
 		for (int r = 0; r < INV_ROWS; r++) {				//For all rows in inventory
 			for (int c = 0; c < INV_COLUMNS; c++) {			//For all cols in inventory
 				if (r == 1) {
 					hotBarOffSet = 200;
 				}
-				inventory[r,c]=blank;						//Set to empty
-				visible[r,c]= Instantiate(emptyImage);
-				visible [r, c].transform.SetParent (this.gameObject.transform);
-				visible [r, c].GetComponent<RectTransform> ().localScale = Vector3.one;
-				visible [r, c].GetComponent<RectTransform> ().localPosition = Vector3.one;
-				visible [r, c].GetComponent<RectTransform> ().localPosition = 
-					newVector((c*100)+0.0f-(WIDTH*0.45f),(hotBarOffSet+r*100)+0.0f-(HEIGHT*0.40f),5.0f);			
-				//visible[r,c].GetComponent<RectTransform> ().position.Set (1.0f, 1.0f, 1.0f);
-				//visible [r, c].transform.localPosition.Set (1.0f, 1.0f, 1.0f);
+				blank.ammount = 0;
+				blank.id = 0;
+				blank.obj = Instantiate(emptyImage);
+				blank.obj.GetComponent<onClickItem> ().parent = this;
+				blank.obj.GetComponent<onClickItem> ().row = r;
+				blank.obj.GetComponent<onClickItem> ().col = c;
+				inventory[r,c]=blank;																		//Set to empty
+				inventory [r, c].obj.transform.SetParent (this.gameObject.transform);						//Position within Parent
+				inventory [r, c].obj.GetComponent<RectTransform> ().localScale = Vector3.one;				//Set Scale to normal	
+				inventory [r, c].obj.GetComponent<RectTransform> ().localPosition = Vector3.one;			//Centre Postion
+				inventory [r, c].obj.GetComponent<RectTransform> ().localPosition = 
+					newVector((c*100)+0.0f-(WIDTH*0.45f),(hotBarOffSet+r*100)+0.0f-(HEIGHT*0.40f),5.0f);	//Position In Table form			//TODO Adjust gap sizing by screen size
 			}
 		}
-		//visible [0, 0].transform.localPosition.Set (10.0f, 10.0f, 0.0f); Doesent work/update
-		visible [0, 0].GetComponent<Image>().sprite = SCREWDRIVER_PIC;
-		//---------------------------------------------------------------------------------- Image Set Up
-		/*public const int EMPTYID = 0;					Empty Inventory Slot ID
-
-		public const int SCREWDRIVER = 	1;				ScrewDriver ID
-		public const int PLASMACUTTER = 	2;			PlasmaCutter ID
-		public const int WELDER = 			3;			Welder ID
-		public const int POWERCABLE = 		4;			PowerCable ID
-		public const int CIRCUITBOARD = 	5;			Circuit Board ID
-		public const int SCRAPMETAL = 		6;			Scrap Metal ID
-		public const int SCREWS = 			7;			Screws ID */
-
-		//pictures = new Sprite[7];
 	}
+
+
+
+
     // Update is called once per frame
     void Update() {
 
@@ -150,6 +155,28 @@ public class ItemSwitching : MonoBehaviour {
 
     }
 
+	public void itemClicked(int row,int col){
+		Debug.Log ("Activated  row:" + row + " col: " + col);
+
+		if (options.activeSelf) {			//Options Already Displaued
+			sel_row_2 = row;				//Get Second Selection
+			sel_col_2 = col;
+			//TODO swap();
+			options.SetActive (false);		//Hide options
+		} else {
+			options.SetActive (true);		//Display Options
+			sel_row_1 = row;				//First Selection
+			sel_col_1 = col;
+			options.transform.SetParent(this.gameObject.transform);
+			options.transform.localPosition = Vector3.one;			//Centre Postion
+			//Debug.Log ("Mouse Pos "+Input.mousePosition);
+			options.transform.localPosition = inventory[row,col].obj.GetComponent<RectTransform>().localPosition;
+			//TODO set postion of options
+		}
+
+	}
+
+
 	/*returns true/false if item is found and puts the lococation of the item in row and col
 	 Starts from row, col, ensure intialisation and allows it to find multiple of the same object*/
 	bool findNextItem(int row,int col,int item_id, out int fRow,out int fCol){
@@ -175,17 +202,17 @@ public class ItemSwitching : MonoBehaviour {
 
 		for (int r = INV_ROWS-1; r >=1; r--) {										//For all rows in inventory top to bottom
 			for (int c = INV_COLUMNS-1; c >=0; c--) {								//For all cols in inventory top to bottom
-				if (inventory[r,c].id == EMPTYID) {								//Where item.id == EMPTY
+				if (inventory[r,c].id == EMPTY_ID) {								//Where item.id == EMPTY
 					for (row = row; row < INV_ROWS && floated == false; row++) {				//For all rows in inventory from bottom to top
 						for (col = col; col < INV_COLUMNS && floated == false; col++) {		//for all columns in inventory from bottom to top
 							if (row >= r && (col >= c || row > r)) {						//If Crossed Search Over
 								floated = true;
 								r = 0;
 								c = 0;
-							} else if(inventory[row,col].id != EMPTYID) {					//Where full swap with empty
+							} else if(inventory[row,col].id != EMPTY_ID) {					//Where full swap with empty
 								inventory [r,c].id = inventory [row,col].id;
 								inventory [r,c].ammount = inventory [row,col].ammount;
-								inventory [row,col].id = EMPTYID;
+								inventory [row,col].id = EMPTY_ID;
 								inventory [row,col].ammount = 0;
 								floated = true;
 							}
@@ -202,7 +229,7 @@ public class ItemSwitching : MonoBehaviour {
 	bool insertAtTop(item insert){
 		for (int r = INV_ROWS - 1; r >= 1; r--) {										//For all rows in inventory top to bottom
 			for (int c = INV_COLUMNS - 1; c >= 0; c--) {								//For all cols in inventory top to bottom
-				if (inventory [r,c].id == EMPTYID) {									//Where slot is empty
+				if (inventory [r,c].id == EMPTY_ID) {									//Where slot is empty
 					inventory[r,c] = insert;											//Insert
 					return true;
 				}
---
+++ Solitude/Assets/Scripts/Util/Inventory UI/onClickSwap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Swap : MonoBehaviour {
+
+	private Button self;
+	public ItemSwitching parent;
+
+	// Use this for initialization
+	void Start () {
+		self = this.GetComponent<Button>();
+		self.onClick.AddListener (pressed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void pressed(){
+		Debug.Log ("Button Pressed Swap");
+		//parent.swap();
+	}
+}
