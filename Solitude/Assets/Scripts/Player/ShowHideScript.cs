f91e1f1696fbc6c4ee2911ad2ff19aec80ecedce
warglaivez <nieldj@utas.edu.au>
2017-09-20 13:24:03 +1000

    tutorial in pause settings works

---
+++ Solitude/Assets/Scripts/Player/ShowHideScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHideScript : MonoBehaviour {
+
+    public GameObject window;
+
+
+    public void Show()
+    {
+        window.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        window.SetActive(false);
+    }
+
+}
