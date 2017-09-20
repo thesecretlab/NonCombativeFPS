bb5de2415280a95354ca52e9e0f6ae50356e50d2
warglaivez <nieldj@utas.edu.au>
2017-09-20 22:39:58 +1000

    Created in game timer for win state viewable on bridge screen

---
+++ Solitude/Assets/Scripts/GameConditions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameConditions : MonoBehaviour {
+
+    public float Time = 600.0f;
+    public Text text;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Time -= UnityEngine.Time.deltaTime;
+        text.text = "Time \nRemaining:  " + Mathf.Round(Time);
+    }
+}
