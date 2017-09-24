4ef28a33acb6c9fd0c460c52c930b4af1e8fa503
warglaivez <nieldj@utas.edu.au>
2017-09-24 14:36:46 +1000

    health decrease cryo

--- Solitude/Assets/Scripts/Terminals/CryoTerminalScript.cs
+++ Solitude/Assets/Scripts/Terminals/CryoTerminalScript.cs
@@ -8,9 +8,12 @@ public class CryoTerminalScript : Terminal
 {
     public GameObject[] crew;
     GameObject crew_power;
+    GameObject crew_health;
     Text crew_power_text;
-    Text crew_health;
+    Text crew_health_text;
     bool powervalue = false;
+    float CrewHealthNum = 100.0f;
+    float decreasehealthRate = 0.01f;
 
     public override void interact()
     {
@@ -20,14 +23,35 @@ public class CryoTerminalScript : Terminal
             crew_power_text = crew_power.GetComponent<Text>();
 
             if (powervalue == false) {
-                crew_power_text.text = ("TEST");
+                crew_power_text.text = ("OFFLINE");
             }
+
+            if (powervalue == true)
+            {
+                crew_power_text.text = ("ONLINE");
+            }
+
         }
     }
 
     protected override void doUpdate()
     {
+        foreach (GameObject c in crew)
+        {
+            crew_health = c.transform.Find("HEALTH").gameObject;
+            crew_health_text = crew_health.GetComponent<Text>();
 
+            if(powervalue == false)
+            {
+                CrewHealthNum = CrewHealthNum - 0.01f;
+                crew_health_text.text = Mathf.Floor(CrewHealthNum) + "%";
+            }   
+            else if (CrewHealthNum <= 100)
+            {
+                CrewHealthNum += 0.05f;
+               
+            }
+        }
     }
 
     protected override void initialise()
