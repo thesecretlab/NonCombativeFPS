edc1eed52d541d6fe93c7069e7632c4099264632
radioactiv327 <brendanweeding@gmail.com>
2017-09-27 13:54:10 +1000

    Crates now give items, limited to once per pickup
    Crates now give one or two items when interacted with.
    This interaction is limited to once per crate being picked up

--- Solitude/Assets/Scripts/Util/CargoRoomAnim.cs
+++ Solitude/Assets/Scripts/Util/CargoRoomAnim.cs
@@ -9,56 +9,50 @@ public class CargoRoomAnim : MonoBehaviour
     int nextCrate;
     AudioSource source;
 
+    public CrateGiveItem crate1;
+    public CrateGiveItem crate2;
+    public CrateGiveItem crate3;
+    public CrateGiveItem crate4;
+
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         source = GetComponent<AudioSource>();
-        //source.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         source.volume = (PlayerPrefs.GetFloat("SFXSound"));
-
-        /*
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Default State") ||
-            anim.GetCurrentAnimatorStateInfo(0).IsName("Crate 1 Resting") ||
-            anim.GetCurrentAnimatorStateInfo(0).IsName("Crate 2 Resting") ||
-            anim.GetCurrentAnimatorStateInfo(0).IsName("Crate 3 Resting") ||
-            anim.GetCurrentAnimatorStateInfo(0).IsName("Crate 4 Resting")
-            )
-        {
-            source.enabled = false;
-        }
-        */
     }
 
-
+    //used to determine the correct crate to act on, and the correct action to perform
     public void playAnim()
     {
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("Default State"))
         {
-            //source.enabled = true;
-            //source.Play();
-            nextCrate = Random.Range(1, 4);
+            nextCrate = Random.Range(1,1);
             switch (nextCrate)
             {
                 case 1:
                     anim.SetTrigger("Crate1Fetch");
+                    crate1.canGiveItem = true;
                     Debug.Log("crate1");
                     break;
                 case 2:
                     anim.SetTrigger("Crate2Fetch");
+                    crate2.canGiveItem = true;
                     Debug.Log("crate2");
                     break;
                 case 3:
                     anim.SetTrigger("Crate3Fetch");
+                    crate3.canGiveItem = true;
                     Debug.Log("crate3");
                     break;
                 case 4:
                     anim.SetTrigger("Crate4Fetch");
+                    crate4.canGiveItem = true;
                     Debug.Log("crate4");
                     break;
             }
@@ -70,8 +64,6 @@ public class CargoRoomAnim : MonoBehaviour
             anim.GetCurrentAnimatorStateInfo(0).IsName("Crate 4 Resting")
             )
         {
-            //source.enabled = true;
-           // source.Play();
             switch (nextCrate)
             {
                 case 1:
---
+++ Solitude/Assets/Scripts/Util/CrateGiveItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateGiveItem : Interactable {
+
+    public Inventory inv;
+    public bool canGiveItem = true;
+
+    protected override void setup()
+    {
+    }
+
+    public override void interact()
+    {
+        if (canGiveItem)
+        {
+            inv.giveRandomItem(1, 2);
+            canGiveItem = false;
+        }
+    }
+}
