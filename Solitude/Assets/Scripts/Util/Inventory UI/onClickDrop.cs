6f55d8e45765eb0ddc97915a22736d539c4704bc
Alex <AlexTheNinja007@hotmail.com>
2017-10-13 16:59:22 +1100

    2: Inventory OnClicks Comments
    Added Comments to OnClick scripts relating to the Inventory for refrence document autogeneration.

--- Solitude/Assets/Scripts/Util/Inventory UI/onClickDrop.cs
+++ Solitude/Assets/Scripts/Util/Inventory UI/onClickDrop.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+/// <summary>
+/// Used to call the Inventory.drop() function when the player clicks the button this script is attached to.
+/// Attached to Drop button in the Options prefab within Unity.
+/// </summary>
 public class onClickDrop : MonoBehaviour {
 
+	///The Buttton component of the GameObject
 	private Button self;
+	///The Game Inventory to call Inventory.drop() function when clicked
 	public Inventory parent;
 
-	// Use this for initialization
+	/// <summary>
+	/// Initialises the script setting self to the button component and adding a listener for the button
+	/// to call pressed() .
+	/// </summary>
 	void Start () {
 		self = this.GetComponent<Button>();
 		self.onClick.AddListener (pressed);
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
-
+	/// <summary>
+	/// Called when the button is pressed.
+	/// Calls Inventory.drop().
+	/// </summary>
 	void pressed(){
 		//Debug.Log ("Button Pressed Drop");
 		parent.drop (-1,-1);
--- Solitude/Assets/Scripts/Util/Inventory UI/onClickItem.cs
+++ Solitude/Assets/Scripts/Util/Inventory UI/onClickItem.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+/// <summary>
+/// Used to call the Inventory.itemClicked() function when the player clicks the button this script is attached to.
+/// This script is used as a part of the player inventory. It is attached to each item in the inventory and set up by Inventory
+/// so that when the player clicks an item; this script sends the row and column information to the Inventory so it can perform 
+/// opperations.
+/// </summary>
 public class onClickItem : MonoBehaviour {
 
-	private Button self; 
+	///The button the script will add a listner to.
+	private Button self;
+	///Corrisponding row position in the inventory
 	public int row;
+	///Corrisponding column position in the inventory
 	public int col;
+	///Inventory used to call Inventory.itemClicked() 
 	public Inventory parent;
 
-	// Use this for initialization
+	/// <summary>
+	/// Initialises the script setting self to the button component and adding a listener for the button
+	/// to call pressed() .
+	/// </summary>
 	void Start () {
 		self = this.GetComponent<Button>();
 		self.onClick.AddListener (pressed);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+	/// <summary>
+	/// Called when the button is pressed.
+	/// Calls Inventory.itemClicked() with row and col as the parameters 
+	/// </summary>
 	void pressed(){
 		Debug.Log ("Button Pressed row:" + row + " col: " + col);
 		parent.itemClicked(row,col);
