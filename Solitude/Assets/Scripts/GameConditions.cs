6f4346b4be4a997383852165932976138ca798fa
warglaivez <nieldj@utas.edu.au>
2017-10-15 23:31:31 +1100

    fixed cryo and gamecondition scripts

--- Solitude/Assets/Scripts/GameConditions.cs
+++ Solitude/Assets/Scripts/GameConditions.cs
@@ -3,59 +3,73 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-/*
- * Controls and displays the win and lose condition for the game which is a timer.
- * Displays the countdown timer on a canvas
- * 
- *Created by Jonothan
- *Modified by Alexander Tilley 23/09/2017
- *
- */
+///<summary>
+///Controls and displays the win and lose condition for the game which is a timer.
+///Displays the countdown timer on a canvas
+/// 
+///Created by Jonothan Nield
+///Modified by Alexander Tilley 23/09/2017
+///
+///<summary>
 public class GameConditions : MonoBehaviour {
 
+	///static GameConditions variable
     static GameConditions gamecond;
-
+	///Bool variable for ship speed true or false
     public bool gotspeed = false;
+	///Bool variable for ship navigations true or false
     public bool traveling = true;
-
-    public float shiphealth = 100;              //variable that holds ship health value
-    public float TurretAccuracy = 60f;          //variable that holds turret accuracy value
-    float AsteroidHitChance;                    //variable that holds asteroid hitchance
-    bool AsteroidOccurance = false;             //boolean value that determines if an asteroid event occurs
-    public float GameTime = 600.0f;             //Minimum game time variable
-    public Text TimeLefttext;                   //Text that displays time left on bridge screen.
-    public Text ShipHealthtext;                 //Text to display on bridge screen to update time
-    bool GameoverCrewdead;                      //boolean value game over if crew dead
-    bool GameoverShipdead;                      //boolean value game over if ship dead
-    public GameObject EndGameWindow;            //Window that displays the end game screen
-    public AudioClip Explosionclip;             //Audio file to be played for explosion
-    public AudioSource Explosionsource;         //source of the audio for explosion
+	///variable that holds ship health value
+    public float shiphealth = 100;  
+	///variable that holds turret accuracy value	
+    public float TurretAccuracy = 60f;  
+	///variable that holds asteroid hitchance	
+    float AsteroidHitChance;   
+	///boolean value that determines if an asteroid event occurs	
+    bool AsteroidOccurance = false;   
+	///Minimum game time variable	
+    public float GameTime = 600.0f;        
+	///Text that displays time left on bridge screen.	
+    public Text TimeLefttext;         
+	///Text to display on bridge screen to update time	
+    public Text ShipHealthtext;   
+	///boolean value game over if crew dead	
+    bool GameoverCrewdead;  
+	///boolean value game over if ship dead	
+    bool GameoverShipdead;          
+	///Window that displays the end game screen	
+    public GameObject EndGameWindow; 
+	///Audio file to be played for explosion	
+    public AudioClip Explosionclip;  
+	///source of the audio for explosion	
+    public AudioSource Explosionsource;         
 
     
 
-    public Text EndGameText;					//Text Object to display at endgame
+	///Text Object to display at endgame
+    public Text EndGameText;					
 
-    //function sets the accuracy of the asteroid defence system 
-    //function for other objects to call to set the fields of the static object
+    ///function sets the accuracy of the asteroid defence system 
+    ///function for other objects to call to set the fields of the static object
     public static void setAccuracy(float acc) {
         gamecond.TurretAccuracy = acc;
     }
 
-    //fucntion sets the ship to be "travelling" which means the game time counts down
-    //function for other objects to call to set the fields of the static object
+    ///fucntion sets the ship to be "travelling" which means the game time counts down
+    ///function for other objects to call to set the fields of the static object
     public static void setTraveling(bool travel) {
         gamecond.traveling = travel;
     }
 
-    //functions sets crew to be dead, forcing game over condition.
-    //function for other objects to call to set the fields of the static object
+    ///functions sets crew to be dead, forcing game over condition.
+    ///function for other objects to call to set the fields of the static object
     public static void allDead() {
         gamecond.GameoverCrewdead = true;
     }
 
 
-    // on awake,sets itself as the static gamecondition, or deletes itself if another one is found
-    // the static allows it to be directly referenced by other scripts
+    /// on awake,sets itself as the static gamecondition, or deletes itself if another one is found
+    /// the static allows it to be directly referenced by other scripts
     void Awake() {
         if (gamecond == null) {
             gamecond = this;
@@ -63,32 +77,35 @@ public class GameConditions : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-    // Use this for initialization
+    /// Use this for initialization
     void Start ()
-    {
+    {	///debugging purposes
         Debug.Log(name);
-        ShipHealthtext.text = "Hull Integrity " + shiphealth + " %";    //text to be displayed on bridge screen.
-        Explosionsource.clip = Explosionclip;                           //sets up audio file for asteroid hits/explosions
+		///text to be displayed on bridge screen.
+        ShipHealthtext.text = "Hull Integrity " + shiphealth + " %"; 
+		///sets up audio file for asteroid hits/explosions
+        Explosionsource.clip = Explosionclip;                       
 
     }
 	
-	// Update is called once per frame
+	/// Update is called once per frame
 	void Update ()
-    {
-        AsteroidHitChance = (100 - TurretAccuracy);                     //sets asteroid hit chance to be that of 100 minus turret accuracy
-        AsteroidChance();                                               //asteroidchance function called, (handles asteroid event occurance)
-
-        if(AsteroidOccurance == true && shiphealth >= 0)                //if an asteroid event occurs and ship health is greater or equal to 0
-        {
-            float random1 = Random.Range(0, 1000);                      //Select a value at random between the given number range
-            {
-                if(random1 >= 0 && random1 <= AsteroidHitChance)        //if selected value is greater/equal to 0  and less than/equal to asteroidhitchance varaible
-                {
-                    float random2 = Random.Range(1, 7);                 //select a value at random between the given number range.
-                    {
-                        shiphealth = shiphealth - random2;              //ship health is equal to ship health minus the value of the random2 variable (taking damage)
-                        
-                        Explosionsource.Play();                         //plays the explosion audio clip attached to in game object
+    {   ///sets asteroid hit chance to be that of 100 minus turret accuracy
+        AsteroidHitChance = (100 - TurretAccuracy);   
+		///asteroidchance function called, (handles asteroid event occurance)		
+        AsteroidChance();                                             
+       ///if an asteroid event occurs and ship health is greater or equal to 0
+        if(AsteroidOccurance == true && shiphealth >= 0)         
+        {   ///Select a value at random between the given number range
+            float random1 = Random.Range(0, 1000);              
+            {   ///if selected value is greater/equal to 0  and less than/equal to asteroidhitchance varaible
+                if(random1 >= 0 && random1 <= AsteroidHitChance)   
+                {   ///select a value at random between the given number range.
+                    float random2 = Random.Range(1, 7);            
+                    {   ///ship health is equal to ship health minus the value of the random2 variable (taking damage)
+                        shiphealth = shiphealth - random2;          
+                        ///plays the explosion audio clip attached to in game object 
+                        Explosionsource.Play();                     
                         
                     }
                     
@@ -96,70 +113,91 @@ public class GameConditions : MonoBehaviour {
             }
         }
         ShipHealthtext.text = "";
-        if (TurretAccuracy < 50) {                                                      //If turretaccuracy is less than
-            ShipHealthtext.text = "Defence Accuracy Low\nRecalibration Necessary\n";    //Display text on bridge screen
+		 ///If turretaccuracy is less than 50
+        if (TurretAccuracy < 50) {                
+			///Display text on bridge screen		
+            ShipHealthtext.text = "Defence Accuracy Low\nRecalibration Necessary\n";    
         }
-        ShipHealthtext.text += "Hull Integrity " + shiphealth + " %";                   //Display text on bridge screen
-
-        if (traveling) {                                                                //if navigation is working
-            if (gotspeed) {                                                             //if FTL has power
-                GameTime -= Time.deltaTime;                                             //Countdown GameTime value
-                TimeLefttext.text = "Time Remaining:  " + Mathf.Round(GameTime);        //Display value of GameTime on the bridge screen in game. 
+		///Display text on bridge screen
+        ShipHealthtext.text += "Hull Integrity " + shiphealth + " %";                   
+		///if navigation is working
+        if (traveling) {    
+			///if FTL has power		
+            if (gotspeed) { 
+				///Countdown GameTime value			
+                GameTime -= Time.deltaTime;     
+				///Display value of GameTime on the bridge screen in game. 				
+                TimeLefttext.text = "Time Remaining:  " + Mathf.Round(GameTime);        
             } else {
-                TimeLefttext.text = "Power Failure";                                    //Display only the text "Power Failure"
+				///Display only the text "Power Failure"
+                TimeLefttext.text = "Power Failure";                                    
             }
         } else {
-            TimeLefttext.text = "Navigation Error";                                     //Display only the text "Navigation Error"
+			///Display only the text "Navigation Error"
+            TimeLefttext.text = "Navigation Error";                                     
         }
         
-
-        if(GameTime < 0)										//Player Wins
+		///Player Wins
+        if(GameTime < 0)										
         {
-			EndGameWindow.SetActive(true);						//Display EndGame Window
+			///Display EndGame Window
+			EndGameWindow.SetActive(true);	
+			///disables all in game frames				
             Player.playerObj.FPSEnable(false);
         }
-        if(GameoverCrewdead)                                    //Player loses game over crew dead.
+		///Player loses game over crew dead.
+        if(GameoverCrewdead)                                    
         {
-            loseGame("The crew are dead");                      //Call lose game function, display text
+			///Call lose game function, display text
+            loseGame("The crew are dead");                      
         }
-
-        if(shiphealth <= 0)                                      //player loses game over ship dead.
+		///player loses game over ship dead.
+        if(shiphealth <= 0)                                      
         {
-            loseGame("The ship has been destroyed");            //Call lose game function, display text
+			///Call lose game function, display text
+            loseGame("The ship has been destroyed");            
         }
     }
 
-    // functions handles wether an asteroid event occurs or not, picking a value from a random number range. 
+    /// functions handles wether an asteroid event occurs or not, picking a value from a random number range. 
     void AsteroidChance()
     {
-        float random = Random.Range(0, 10000);      //Select random value and assign to variable between specified number range
-        {
-            if (random < 65 && random > 50)         //If selected random value is between specified number range
+		///Select random value and assign to variable between specified number range
+        float random = Random.Range(0, 10000);      
+        {	///If selected random value is between specified number range
+            if (random < 65 && random > 50)         
             {
-                AsteroidOccurance = true;           //Asteriod event occurs (is true)
+				///Asteriod event occurs (is true)
+                AsteroidOccurance = true;           
             }
             else
             {
-                AsteroidOccurance = false;          //Asteroid event doesnt occur (is false)
+				///Asteroid event doesnt occur (is false)
+                AsteroidOccurance = false;          
             }
         }
 
     }
 
-	//Triggers lose game state and displays addtional text if needed
+	///Triggers lose game state and displays addtional text if needed
 	public void loseGame(string optional){
-		if (optional == null) {				//If nothing was entred
+		///If nothing was entred
+		if (optional == null) {				
 			optional = "";
 		}
-		EndGameText.color = Color.red;                  //sets endgame color to red
-		EndGameText.text = "Game Over \n" + optional;	//Sets endgame text to display game over plus optional text depending on the game over condition
-		EndGameWindow.SetActive(true);                  //enables engame window
-		Player.playerObj.FPSEnable(false);              //disables all in game frames
+		///sets endgame color to red
+		EndGameText.color = Color.red;       
+		///Sets endgame text to display game over plus optional text depending on the game over condition		
+		EndGameText.text = "Game Over \n" + optional;
+		///enables engame window		
+		EndGameWindow.SetActive(true);     
+		///disables all in game frames		
+		Player.playerObj.FPSEnable(false);              
 
 	}
 
-    //sets speed of the ship
-    //function for other objects to call to set the fields of the static object
+    ///sets speed of the ship
+    ///function for other objects to call to set the fields of the static object
     public static void setSpeed(bool v) {
         gamecond.gotspeed = v;
     }
--- Solitude/Assets/Scripts/Terminals/CryoTerminalScript.cs
+++ Solitude/Assets/Scripts/Terminals/CryoTerminalScript.cs
@@ -4,43 +4,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/*
- *Handles all elements associated with the cryo terminal user interface
- */
+/// <summary>
+///Handles all elements associated with the cryo terminal user interface
+/// <summary>
 public class CryoTerminalScript : Terminal, PowerConsumer
 {
+	
+	/// array of gameobjects handling all crew objects
+    public GameObject[] crew;    
+	///Game object handling the crew power
+    GameObject crew_power;  
+	///Game object handling the crew health	
+    GameObject crew_health;   
+	///text that displays power offline or online status for each crew member	
+    Text crew_power_text;   
+	///text that displays the health from 0 to 100 for the crew members	
+    Text crew_health_text;   
+	///bool to handle power on or off status	
+    bool powervalue = false;   
+	///bool to handle crew dead or alive status	
+    bool crewdead = false;  
+	///bool to handle if all crew are dead or not. 	
+    public bool allcrewdead = false;  
+	///variable that holds the health value of the crew member	
+    float CrewHealthNum = 100.0f;    
+	///counter used in counting crew members	
+    int crewcount = 0;                  
+	///value represents the Power required to power the cryoroom
+    int needPower = 11; 
 
-    public GameObject[] crew;           // array of gameobjects handling all crew objects
-    GameObject crew_power;              //Game object handling the crew power
-    GameObject crew_health;             //Game object handling the crew health
-    Text crew_power_text;               //text that displays power offline or online status for each crew member
-    Text crew_health_text;              //text that displays the health from 0 to 100 for the crew members
-    bool powervalue = false;            //bool to handle power on or off status
-    bool crewdead = false;              //bool to handle crew dead or alive status
-    public bool allcrewdead = false;    //bool to handle if all crew are dead or not. 
-    float CrewHealthNum = 100.0f;       //variable that holds the health value of the crew member
-    int crewcount = 0;                  //counter used in counting crew members
-
-    int needPower = 11; //value represents the Power required to power the cryoroom
-
-    //Called when player interacts with the in game terminal object.
+    ///Called when player interacts with the in game terminal object.
     public override void interact()
     {
-        //loads the cryo terminal user interface
+        ///loads the cryo terminal user interface
         show();
 
-        //Searches through all gameobjects in the heiarchy of the cryoterminal scene that are named "POWER"
-        //and grabs the text component.
+        ///Searches through all gameobjects in the heiarchy of the cryoterminal scene that are named "POWER"
+        ///and grabs the text component.
         foreach (GameObject c in crew) {
             crew_power = c.transform.Find("POWER").gameObject;
             crew_power_text = crew_power.GetComponent<Text>();
 
-            //if the cryoroom has no power set crew power gameobject text to display OFFLINE
+            ///if the cryoroom has no power set crew power gameobject text to display OFFLINE
             if (powervalue == false) {
                 crew_power_text.text = ("OFFLINE");
             }
 
-            //if the cryoroom has power set crew power gameobject text to display ONLINE
+            ///if the cryoroom has power set crew power gameobject text to display ONLINE
             if (powervalue == true)
             {
                 crew_power_text.text = ("ONLINE");
@@ -49,50 +59,54 @@ public class CryoTerminalScript : Terminal, PowerConsumer
         }
     }
 
-    //doUpdate updates the user interface in real time according to frames per second. 
+    ///doUpdate updates the user interface in real time according to frames per second. 
     protected override void doUpdate()
     {
-        //Searches through all gameobjects in the heiarchy of the cryoterminal scene that are named "HEALTH"
-        //and grabs the text component.
+        ///Searches through all gameobjects in the heiarchy of the cryoterminal scene that are named "HEALTH"
+        ///and grabs the text component.
         foreach (GameObject c in crew)
         {
             crew_health = c.transform.Find("HEALTH").gameObject;
             crew_health_text = crew_health.GetComponent<Text>();
 
-            //if there is now power to the cryo room and all the crew are not dead
+            ///if there is now power to the cryo room and all the crew are not dead
             if (powervalue == false && allcrewdead == false)
             {
-                //if crew members health is greater than zero
+                ///if crew members health is greater than zero
                 if(CrewHealthNum > 0.0f)
                 {
-                    //decrease the crew members health by 0.0001f and update the crew health text to display the current value
+                    ///decrease the crew members health by 0.0001f and update the crew health text to display the current value
                     CrewHealthNum = CrewHealthNum - 0.0001f;
                     crew_health_text.text = Mathf.Floor(CrewHealthNum) + "%";
                 }
                 else
                 {
-                    Toast.addToast("Crew member died", 3); //Text is displayed via pop up to the player HUD
-                    crew_health_text.text = "DECEASED"; //changes the health text of the associated crew member to deceased. 
-                    crewdead = true; //crew member has died.
-                    crewcount++; //crew member has died, add to crew count variable. 
+					///Text is displayed via pop up to the player HUD
+                    Toast.addToast("Crew member died", 3); 
+					///changes the health text of the associated crew member to deceased. 
+                    crew_health_text.text = "DECEASED"; 
+					///crew member has died.
+                    crewdead = true; 
+					///crew member has died, add to crew count variable.
+                    crewcount++;  
 
                 }
             }
 
-            //if crew count has reached 41
+            ///if crew count has reached 41
             if(crewcount == 41) 
             {
-                //call the function allDead from the gameconditions script. which will cause a gameover state to occur. 
+                ///call the function allDead from the gameconditions script. which will cause a gameover state to occur. 
                 GameConditions.allDead();
             }
             
-            //if cryo room has power but all the crew are not dead.
+            ///if cryo room has power but all the crew are not dead.
             if (powervalue == true && allcrewdead == false)
             {
-                //if crew members health less than 100
+                ///if crew members health less than 100
                 if(CrewHealthNum < 100)
                 {
-                    //incrememnt crew health value by 0.00005f and update the crew health text to display the current value
+                    ///incrememnt crew health value by 0.00005f and update the crew health text to display the current value
                     CrewHealthNum = CrewHealthNum + 0.00005f;
                     crew_health_text.text = Mathf.Floor(CrewHealthNum) + "%";
                 }
@@ -101,15 +115,16 @@ public class CryoTerminalScript : Terminal, PowerConsumer
         }
     }
 
-    //initialization function 
+    ///initialization function 
     protected override void initialise()
     {
 
-        //creates a list of all crew members in the cryo scene hierarchy 
+        ///creates a list of all crew members in the cryo scene hierarchy 
         List<GameObject> cl = new List<GameObject>();
         foreach (Text go in ui.GetComponentsInChildren<Text>())
         {
-            if (go.name.Contains("Crew")) //add gameobject to list if "crew" is somewhere in its name. 
+			///add gameobject to list if "crew" is somewhere in its name. 
+            if (go.name.Contains("Crew")) 
             {
                 cl.Add(go.gameObject);
             }
@@ -118,13 +133,14 @@ public class CryoTerminalScript : Terminal, PowerConsumer
 
     }
 
+	///Unused function at this time
     protected override void onClose()
     {
 
     }
 
-    //the function is called when the power system changes the amount of power delivered to that object. 
-    //Inherited from the power consumer interface
+    ///the function is called when the power system changes the amount of power delivered to that object. 
+    ///Inherited from the power consumer interface
     public void updatePower(bool powered) {
         String toast = powered ? "Cryo Power Restored" : "WARNING! Cryo Power Failure";
         Toast.addToast(toast, 3);
