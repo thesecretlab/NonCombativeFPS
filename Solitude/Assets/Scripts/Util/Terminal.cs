85a327725ad106ab21f69fdaf67f039f9e2d9ec8
jpalbion <jpalbion@utas.edu.au>
2017-10-20 00:16:49 +1100

    Second set of commits

--- Solitude/Assets/Scripts/Util/Terminal.cs
+++ Solitude/Assets/Scripts/Util/Terminal.cs
@@ -2,17 +2,55 @@
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-
+/// 
+/// \brief An inheritable class for all terminal objects
+/// 
 public abstract class Terminal : Interactable, Window {
-	
+	/// The prefab for the UI
     public GameObject uiPrefab;
+	/// The UI created from the prefab
     protected GameObject ui;
+    /// 
+    /// \brief Used by inheritors for initialisation
+    /// 
+    /// \return No return value
+    /// 
+    /// \details 
+    /// 
     protected abstract void initialise();
+    /// 
+    /// \brief Used by inheritors for Update
+    /// 
+    /// \return No return value
+    /// 
+    /// \details 
+    /// 
     protected abstract void doUpdate();
+    /// 
+    /// \brief Used buy inheritors for interaction
+    /// 
+    /// \return No return value
+    /// 
+    /// \details 
+    /// 
     public override abstract void interact();
+	/// If the attached UI is visible
     protected bool isVis;
+    /// 
+    /// \brief Used by inheritors for when the ui iis closing
+    /// 
+    /// \return No return value
+    /// 
+    /// \details 
+    /// 
     protected abstract void onClose();
-
+    /// 
+    /// \brief Used for initialisation
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Creates the UI form prefab
+    /// 
     protected override void setup() {
         ui = Instantiate(uiPrefab);
         //Debug.LogWarning(name + ":" + uiPrefab.name);
@@ -21,9 +59,23 @@ public abstract class Terminal : Interactable, Window {
         ui.SetActive(false);
         isVis = false;
     }
+    /// 
+    /// \brief Called once per frame
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Calls inheritors update
+    /// 
     void Update() {
         doUpdate();
     }
+    /// 
+    /// \brief Shows the UI
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Disables the FPSControler and enables the mouse
+    /// 
     protected void show() {
         Debug.Log("Open " + name);
         ui.SetActive(true);
@@ -31,10 +83,23 @@ public abstract class Terminal : Interactable, Window {
         Player.playerObj.FPSEnable(false);
         isVis = true;
     }
+    /// 
+    /// \brief Hides the UI
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Gets the player object to close the window
+    /// 
     public void hide() {
         Player.closeWindow();
     }
-
+    /// 
+    /// \brief Called when the window is closed
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Enables theFPSControler and disables the mouse
+    /// 
     public void close() {
         onClose();
         Player.playerObj.FPSEnable(true);
