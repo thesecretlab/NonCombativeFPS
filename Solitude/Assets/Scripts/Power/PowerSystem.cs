f2d8a1e59b289e7bfe514fbe725e4fe9b36acc6d
jpalbion <jpalbion@utas.edu.au>
2017-10-19 16:20:38 +1100

    first comments

--- Solitude/Assets/Scripts/Power/PowerSystem.cs
+++ Solitude/Assets/Scripts/Power/PowerSystem.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// 
+/// \file PowerSystem.cs
+/// \brief PowerSystem is used to receive and route power to different classes that require it.
+/// 
 public class PowerSystem : MonoBehaviour {
 
     [System.Serializable]
     public struct RoomLevels {
         public Room[] rooms;
         public int[] levels;
-
+        /// 
+        /// \brief Constructor For a Roomlevels struct
+        /// 
+        /// \param [in] r An array of room objects
+        /// \param [in] start An integer defining the starting power level for all rooms
+        /// \return Returns a RoomLevels object
+        /// 
         public RoomLevels(Room[] r,int start) {
             this.rooms = r;
             List<int> l = new List<int>();
@@ -19,7 +29,13 @@ public class PowerSystem : MonoBehaviour {
             }
             levels = l.ToArray();
         }
-
+        /// 
+        /// \brief Used to crash the rooms power
+        /// 
+        /// \return No return value
+        /// 
+        /// \details Sets the powerlevel of all rooms in the array to 0
+        /// 
         public void crash() {
             int i = 0;
             foreach (Room r in rooms) {
@@ -27,7 +43,13 @@ public class PowerSystem : MonoBehaviour {
                 i++;
             }
         }
-
+        /// 
+        /// \brief Used to restore the power to all rooms
+        /// 
+        /// \return No return value
+        /// 
+        /// \details Resets the powerlevel of all rooms to the stored value
+        /// 
         public void restart() {
             for (int i = 0; i < rooms.Length; i++) {
                 rooms[i].setPower(levels[i]);
@@ -40,7 +62,14 @@ public class PowerSystem : MonoBehaviour {
                 levels[i] = rooms[i].changePower(false);
             }
         }*/
-
+        /// 
+        /// \brief Gets the minimum power level for the room
+        /// 
+        /// \param [in] room The name of the room as a String
+        /// \return Returns the minimum power as an integer
+        /// 
+        /// \details The minimum power of the room is the lowest powerlevel that the powerd objects in that room require to function
+        /// 
         public int getMin(string room) {
             for (int i = 0; i < rooms.Length; i++) {
                 if (rooms[i].name.ToLower() == room.ToLower()) {
@@ -49,7 +78,16 @@ public class PowerSystem : MonoBehaviour {
             }
             return -1;
         }
-
+        /// 
+        /// \brief Increases or degreases the power to a room
+        /// 
+        /// \param [in] room The name of the room as a String
+        /// \param [in] up Should the power be increased or decreased, true for increase
+        /// \param [in] max The maximum total power for all rooms
+        /// \return Returns the new powerlevel as an integer
+        /// 
+        /// \details Will return -1 if the room cannot be found
+        /// 
         public int changePower(string room, bool up, int max) {
             for (int i = 0; i < rooms.Length; i++) {
                 if (rooms[i].name.ToLower() == room.ToLower()) {
@@ -68,7 +106,16 @@ public class PowerSystem : MonoBehaviour {
             }
             return -1;
         }
-
+        /// 
+        /// \brief Sets the powerlevel of a room
+        /// 
+        /// \param [in] room The room name as a String
+        /// \param [in] pow The power level to set the room to
+        /// \param [in] max The maximum total power for all rooms
+        /// \return Returns the new powerLevel as an integer
+        /// 
+        /// \details Will return -1 if the room cannot be found
+        /// 
         public int setPower(string room, int pow, int max) {
             for (int i = 0; i < rooms.Length; i++) {
                 if (rooms[i].name.ToLower() == room.ToLower()) {
@@ -80,7 +127,14 @@ public class PowerSystem : MonoBehaviour {
             }
             return -1;
         }
-
+        /// 
+        /// \brief Gets the power level of a room
+        /// 
+        /// \param [in] room The name of the room as a string
+        /// \return Returns the power level of the room
+        /// 
+        /// \details Will return -1 if the room is not found
+        /// 
         public int getPower(string room) {
             for (int i = 0; i < rooms.Length; i++) {
                 if (rooms[i].name.ToLower() == room.ToLower()) {
@@ -89,48 +143,127 @@ public class PowerSystem : MonoBehaviour {
             }
             return -1;
         }
-
+        /// 
+        /// \brief Gets the total used power of all rooms
+        /// 
+        /// \return Returns the total power as an integer
+        /// 
+        /// \details 
+        /// 
         public int total() {
             int s = 0;
             foreach (int i in levels) s += i;
             return s;
         }
     }
-
+	/// Static PowerSystem object used for direct calls
     static PowerSystem powersystem;
+	/// The text object that displays the used and available power
     public Text powerText;
+	/// The ui used to interact with the power system
     GameObject ui;
+	/// An array of TerminalButtons in the ui
     private TerminalButton[] buttons;
 
     #region Static
+    /// 
+    /// \brief Static call to the crash function
+    /// 
+    /// \return Returns 0
+    /// 
+    /// \details 
+    /// 
     public static int crash() {
         return powersystem._crash();
     }
+    /// 
+    /// \brief Static call to the restore function
+    /// 
+    /// \return Returns the total amount of used power
+    /// 
+    /// \details 
+    /// 
     public static int restore() {
         return powersystem._restore();
     }
+    /// 
+    /// \brief Static call to the getRoom function
+    /// 
+    /// \param [in] room The room name as a string
+    /// \return Returns the power level of the room
+    /// 
+    /// \details Used for finding the power level of a room. Will return -1 if the room is not found
+    /// 
     public static int getRoom(string room) {
         return powersystem._getRoom(room);
     }
+    /// 
+    /// \brief Static call to the changeroom function
+    /// 
+    /// \param [in] room The name of the room as a string
+    /// \param [in] up A boolian stating to increase or decrease the power level
+    /// \return Returns the new power level of the room
+    /// 
+    /// \details Used to increase or decrease the power level of a room. Will return -1 if the room is not found
+    /// 
     public static int changeRoom(string room, bool up) {
         return powersystem._changeRoom(room, up);
     }
+    /// 
+    /// \brief Static call to the setRoom function
+    /// 
+    /// \param [in] room The name of the room as a String
+    /// \param [in] power The new power level of the room
+    /// \return Returns the new powerlevel of the room
+    /// 
+    /// \details Used to set the power level of a room. Will return -1 if the room is not found
+    /// 
     public static int setRoom(string room, int power) {
         return powersystem._setRoom(room, power);
     }
+    /// 
+    /// \brief Static call to the setPower function
+    /// 
+    /// \param [in] power The new maximum power
+    /// \return No return value
+    /// 
+    /// \details Used to set the maximum total power usage of all rooms
+    /// 
     public static void setPower(int power) {
         powersystem._setPower(power);
     }
+    /// 
+    /// \brief Static function to set the ui
+    /// 
+    /// \param [in] ui The ui to use
+    /// \return No return value
+    /// 
+    /// \details Sets the UI and finds all needed UI components
+    /// 
     public static void setUI(GameObject ui) {
         powersystem.ui = ui;
         powersystem.powerText = ui.transform.FindChild("AvailablePowerVariable").GetComponent<Text>();
         powersystem.buttons = ui.GetComponentsInChildren<TerminalButton>();
     }
+    /// 
+    /// \brief Static call to the getMin function
+    /// 
+    /// \param [in] room The room name as a String
+    /// \return Returns the minimum power level of the room
+    /// 
+    /// \details Used to find the minimum required power level of a room. Will return -1 if the room is not found
+    /// 
     public static int getMin(string room) {
         return powersystem._getMin(room);
     }
     #endregion
-
+    /// 
+    /// \brief Called on initial creation of the object
+    /// 
+    /// \return No return value
+    /// 
+    /// \details Used to set the static powerSystem object and make it singular by self deleting if one is already set
+    /// 
     void Awake() {
         if (powersystem == null) {
             //Debug.Log("Added Powersystem at: " + gameObject.name);
